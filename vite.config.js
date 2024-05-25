e386e2f9f0583b4df0f6bc1dcb5480d48ab2cc32
danigonlinea <danigonlinea@gmail.com>
2024-05-26 00:36:59 +0200

    more changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/wedding-app/',
+  base: '.',
   plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
